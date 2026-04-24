3d98aeb206a085bb7e76872aea593730f4da9412
Nanzzz-id <luxxstill@gmail.com>
2026-04-24 07:02:00 +0700

    Update Signature.h

--- include/pl/Signature.h
+++ include/pl/Signature.h
@@ -3,13 +3,6 @@
 
 namespace pl {
 namespace signature {
-
-// Cari fungsi Minecraft pakai byte pattern
-// lib_name: nama library (misal "libminecraftpe.so")
-// signature: string hex pattern, "?" = wildcard
-// return: alamat fungsi, atau 0 jika tidak ditemukan
-uintptr_t pl_resolve_signature(const char* signature,
-                                const char* lib_name);
-
+uintptr_t pl_resolve_signature(const char* signature, const char* lib_name);
 } // namespace signature
 } // namespace pl
